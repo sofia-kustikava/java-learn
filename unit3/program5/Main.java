a05d32d350f675924752368c5763ff1ade7e96a0
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-03-31 00:50:29 +0300

    Update Main.java

--- unit3/program5/Main.java
+++ unit3/program5/Main.java
@@ -1,3 +1,7 @@
+//Create a class called Dog containing two Strings: name and says. In
+//main( ), create two dog objects with names “spot” (who says, “Ruff!”) and “scruffy” (who
+//says, “Wurf!”). Then display their names and what they say.
+
 public class Main {
 
     public static void main(String[] args) {
