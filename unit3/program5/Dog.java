af97b099753574f28b00b8d7ab40017177826276
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-03-30 23:56:57 +0300

    Create Dog.java

---
+++ unit3/program5/Dog.java
@@ -0,0 +1,7 @@
+//Create a class called Dog containing two Strings: name and says. In
+//main( ), create two dog objects with names “spot” (who says, “Ruff!”) and “scruffy” (who
+//says, “Wurf!”). Then display their names and what they say.
+
+public class Dog {
+    public String name,says;
+}
