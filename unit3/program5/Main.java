b266d7009ab97ce68c22a3844d78c372d16ed8c5
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-03-31 00:31:52 +0300

    Update Main.java

--- unit3/program5/Main.java
+++ unit3/program5/Main.java
@@ -1,14 +1,12 @@
-import com.java.se.lesson12.Dog;
-
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog1 = new Dog();
-        Dog dog2 = new Dog();
-        dog1.name = "Spot";
-        dog2.name = "Scruffy";
-        dog1.says = "Ruff!";
-        dog2.says = "Wurf!";
-        System.out.println("The dog named " + dog1.name + " said " + dog1.says + ". But " + dog2.name + " said " + dog2.says);
+        Dog spot = new Dog();
+        Dog scruffy = new Dog();
+        spot.name = "Spot";
+        scruffy.name = "Scruffy";
+        spot.says = "Ruff!";
+        scruffy.says = "Wurf!";
+        System.out.println("The dog named " + spot.name + " said " + spot.says + ". But " + scruffy.name + " said " + scruffy.says);
     }
 }
