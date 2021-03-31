97ac9f769600f0dbc7e9d861126a620580a601b4
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-03-31 14:23:07 +0300

    Update Main.java

--- unit4/program5/Main.java
+++ unit4/program5/Main.java
@@ -1,2 +1,27 @@
 //Repeat Exercise 10 from the previous chapter, using the ternary operator
 //and a bitwise test to display the ones and zeroes, instead of Integer.toBinaryString( ). 
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            BitTest test = new BitTest();
+            int i = 0x0fc;
+            int j = 0x1fd;
+            System.out.println("i = " + i);
+            test.binaryPrint(i);
+            System.out.println("j = " + j);
+            test.binaryPrint(j);
+            System.out.println("i & j = " + (i & j));
+            test.binaryPrint(i & j);
+            System.out.println("i | j = " + (i | j));
+            test.binaryPrint(i | j);
+            System.out.println("i ^ j = " + (i ^ j));
+            test.binaryPrint(i ^ j);
+            System.out.println("~i = " + ~i);
+            test.binaryPrint(~i);
+            System.out.println("~j = " + ~j);
+            test.binaryPrint(~j);
+        }
+  
+}
