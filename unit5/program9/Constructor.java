5009f8871735e4351370b31b9d7a5e484f4ed507
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-02 01:26:40 +0300

    Create Constructor.java

---
+++ unit5/program9/Constructor.java
@@ -0,0 +1,9 @@
+public class Constructor {
+    public Constructor() {
+        this("Sofia", 19);
+    }
+
+    public Constructor(String name, int age) {
+        System.out.println("My name is " + name + ". I'm " + age + ".");
+    }
+}
