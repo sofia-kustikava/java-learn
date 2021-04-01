b6779088cf7035e62ee0d1ca1bdfddfa71cf51fe
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-02 02:03:58 +0300

    Create Main.java

---
+++ unit5/program14/Main.java
@@ -0,0 +1,11 @@
+//Create a class with a static String field that is initialized at the point of
+//definition, and another one that is initialized by the static block. Add a static method that
+//prints both fields and demonstrates that they are both initialized before they are used. 
+
+import com.java.se.lesson12.Static;
+
+public class Main {
+    public static void main(String[] args) {
+        Static.result();
+    }
+}
