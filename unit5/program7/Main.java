c3f1ce0ee4dbaa84adcaf701cbfd0e8df95345e8
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-02 01:13:41 +0300

    Create Main.java

---
+++ unit5/program7/Main.java
@@ -0,0 +1,10 @@
+//Create a class without a constructor, and then create an object of that
+//class in main( ) to verify that the default constructor is automatically synthesized. 
+
+import com.java.se.lesson12.Dog;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog constructor = new Dog();
+    }
+}
