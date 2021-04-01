d029d0a2d4f6a9934726594808b85575be642f7c
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-02 01:24:22 +0300

    Create Main.java

---
+++ unit5/program9/Main.java
@@ -0,0 +1,10 @@
+//Create a class with two (overloaded) constructors. Using this, call the
+//second constructor inside the first one
+
+import com.java.se.lesson12.Constructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Constructor constructor = new Constructor();
+    }
+}
