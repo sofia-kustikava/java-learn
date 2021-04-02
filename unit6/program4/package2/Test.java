22999c5a1703b3b86dafa79a1131c512d97c1061
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 00:43:49 +0300

    Update Test.java

--- unit6/program4/package2/Test.java
+++ unit6/program4/package2/Test.java
@@ -1,1 +1,10 @@
+package com.java.se.package1;
 
+public class Test {
+    protected static void str2() {
+        System.out.println("Protected");
+    }
+    public static void str() {
+        System.out.println("Not Protected");
+    }
+}
