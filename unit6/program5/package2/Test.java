a85ea08e2958509a959d81650f06c8b2ee8bf2b7
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 01:20:59 +0300

    Create Test.java

---
+++ unit6/program5/package2/Test.java
@@ -0,0 +1,19 @@
+package com.java.se.package2;
+
+public class Test {
+
+    public String a = "a";
+    protected String b = "b";
+    private String c = "c";
+
+    public static void str() {
+        System.out.println("Not Protected");
+    }
+    protected static void str2() {
+        System.out.println("Protected");
+    }
+    private static void str3() {
+        System.out.println("Protected");
+    }
+
+}
