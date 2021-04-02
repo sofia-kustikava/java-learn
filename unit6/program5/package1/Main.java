4949032f129f73ec532d1e13cc36be3b9d3c8f2e
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 01:20:22 +0300

    Update Main.java

--- unit6/program5/package1/Main.java
+++ unit6/program5/package1/Main.java
@@ -3,3 +3,20 @@
 //messages you get when you try to access all the class members. Be aware that classes in the
 //same directory are part of the “default” package. 
 
+package com.java.se.package1;
+
+import com.java.se.package2.Test;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Test var = new Test();
+        System.out.println(var.a);
+        System.out.println(var.b);
+        System.out.println(var.c);
+
+        Test.str();
+        Test.str2();
+        Test.str3();
+    }
+}
