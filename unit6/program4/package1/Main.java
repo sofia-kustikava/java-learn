7c92a7acd7070d8e79be8b2f53c829ec0793aca2
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 00:44:11 +0300

    Update Main.java

--- unit6/program4/package1/Main.java
+++ unit6/program4/package1/Main.java
@@ -1,2 +1,12 @@
 //Show that protected methods have package access but are not public
 
+package com.java.se;
+
+import static com.java.se.package1.Test.*;
+
+public class Main {
+    public static void main(String[] args) {
+        str();
+        str2();
+    }
+}
