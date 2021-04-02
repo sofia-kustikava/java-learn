d18eaa610a26e59d0fcbadb75c10ec0f550fb39e
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-02 19:17:52 +0300

    Update Main.java

--- unit6/program1/Main.java
+++ unit6/program1/Main.java
@@ -1,10 +1,16 @@
 //Create a class in a package. Create an instance of your class outside of that
 //package. 
 
-package com.java.second.package3;
+package com.java.se.package1;
 
-public class Edu {
-    public void hello(String a) {
-        System.out.println("Hi! My name is " + a);
+import com.java.second.package3.Edu;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Edu name = new Edu();
+        name.hello("Anya");
     }
+
 }
