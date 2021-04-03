09f128de95709a011503b76877af0e9cb00e8d1c
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 23:24:24 +0300

    Create Root.java

---
+++ unit7/program10/Root.java
@@ -0,0 +1,9 @@
+public class Root {
+    Component1 component1 = new Component1(1);
+    Component2 component2 = new Component2(2);
+    Component3 component3 = new Component3(3);
+    public Root(String string) {
+        System.out.println("Root");
+    }
+
+}
