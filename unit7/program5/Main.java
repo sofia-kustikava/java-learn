824f0246f9ab5f61e9c5be36bc2e5d049f56e684
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 22:47:00 +0300

    Create Main.java

---
+++ unit7/program5/Main.java
@@ -0,0 +1,10 @@
+//Create two classes, A and B, with default constructors (empty argument
+//lists) that announce themselves. Inherit a new class called C from A, and create a member of
+//class B inside C. Do not create a constructor for C. Create an object of class C and observe
+//the results. 
+
+public class Main {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+}
