c40b05f70f5cb426c828c7578124b3b5c954edcc
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 01:53:22 +0300

    Create Frog.java

---
+++ unit7/program17/Frog.java
@@ -0,0 +1,10 @@
+public class Frog extends Amphibian{
+    public String toString() {
+        return "Frog";
+    }
+
+    @Override
+    public void method() {
+        System.out.println("Overriding Frog");
+    }
+}
