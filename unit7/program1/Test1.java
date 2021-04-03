786d0f3f9d04a908f998f06f855a882efd3c4202
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 19:58:43 +0300

    Update Test1.java

--- unit7/program1/Test1.java
+++ unit7/program1/Test1.java
@@ -2,14 +2,15 @@ package com.java.se.package1;
 
 public class Test1 {
     Test2 test2;
-
     public Test1() {
-        test2 = new Test2();
-        System.out.println(test2 + " has been created inside " + this);
+        if (test2 == null) {
+            test2 = new Test2();
+            System.out.println(test2 + " has been created inside " + this);
+        } else {
+            System.out.println("Ops");
+        }
     }
-
     public String toString() {
         return "Test1";
     }
-    
 }
