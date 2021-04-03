01c947bf2f58df770f8dd48a5e094bc28029496d
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 00:20:23 +0300

    Create Main.java

---
+++ unit7/program12/Main.java
@@ -0,0 +1,9 @@
+//Add a proper hierarchy of dispose( ) methods to all the classes in
+//Exercise 9. 
+
+public class Main {
+    public static void main(String[] args) {
+        Stem stem = new Stem("Stem");
+        stem.dispose();
+    }
+}
