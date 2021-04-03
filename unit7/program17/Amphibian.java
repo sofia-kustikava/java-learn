f9954c26601daae7285feca3fdd7576f6de70c6d
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 01:53:40 +0300

    Create Amphibian.java

---
+++ unit7/program17/Amphibian.java
@@ -0,0 +1,9 @@
+public class Amphibian {
+    public void print(Frog amphibian) {
+        System.out.println(amphibian);
+    }
+
+    public void method() {
+        System.out.println("Amphibian");
+    }
+}
