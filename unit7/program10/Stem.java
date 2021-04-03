5ac078e4cd11069c15ff7837b0ec9ef7a2ef708d
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 23:24:44 +0300

    Create Stem.java

---
+++ unit7/program10/Stem.java
@@ -0,0 +1,9 @@
+public class Stem extends Root {
+    Component1 component1 = new Component1(1);
+    Component2 component2 = new Component2(2);
+    Component3 component3 = new Component3(3);
+    public Stem(String string) {
+        super("Root");
+        System.out.println(string);
+    }
+}
