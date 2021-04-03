82c8f0f59185945af6e1302573bec33bac130202
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-03 19:17:17 +0300

    Create Main.java

---
+++ unit7/program1/Main.java
@@ -0,0 +1,10 @@
+//Create a simple class. Inside a second class, define a reference to an object
+//of the first class. Use lazy initialization to instantiate this object. 
+
+package com.java.se.package1;
+
+public class Main {
+    public static void main(String[] args) {
+        Test1 one = new Test1();
+    }
+}
