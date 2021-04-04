58899b2886bcbb2e60ba823ad7c257a5f0138ca0
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 17:12:32 +0300

    Create Test1.java

---
+++ unit8/program10/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    protected void met1() {
+        System.out.println("Method 1");
+        met2();
+    }
+    protected  void met2() {
+        System.out.println("Method 2");
+    }
+}
