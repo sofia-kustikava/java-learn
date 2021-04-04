f5a3f664a01da88c24034fc635d325b8744ebd83
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 17:55:00 +0300

    Create Rodent.java

---
+++ unit8/program12/Rodent.java
@@ -0,0 +1,13 @@
+public class Rodent {
+    public Rodent() {
+        System.out.println("Create Rodent");
+    }
+
+    public void move() {}
+
+    public void gnaw() {}
+
+    public void dispose() {
+        System.out.println("Dispose of Rodent");
+    }
+}
