4681bcd8555654816c8282aa5c087b386f5f472d
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 18:31:46 +0300

    Create Main.java

---
+++ unit8/program15/Main.java
@@ -0,0 +1,8 @@
+//Add a RectangularGlyph to PolyConstructors.java and
+//demonstrate the problem described in this section. 
+
+public class Main {
+    public static void main(String[] args) {
+        new PolyConstructors();
+    }
+}
