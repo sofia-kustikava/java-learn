3bf84bd7001018b7da953f85c06c5c54ed6f38ad
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 18:33:09 +0300

    Create Glyph.java

---
+++ unit8/program15/Glyph.java
@@ -0,0 +1,10 @@
+public class Glyph {
+    void draw() {
+        System.out.println("Glyph.draw()");
+    }
+    Glyph() {
+        System.out.println("Glyph() before draw()");
+        draw();
+        System.out.println("Glyph() after draw()");
+    }
+}
