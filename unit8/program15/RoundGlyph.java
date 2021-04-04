9751979c7ed7c3201c91dad51baae309b0fd0df2
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 18:33:37 +0300

    Create RoundGlyph.java

---
+++ unit8/program15/RoundGlyph.java
@@ -0,0 +1,10 @@
+public class RoundGlyph extends Glyph {
+    private int radius = 1;
+    RoundGlyph(int r) {
+        radius = r;
+        System.out.println("RoundGlyph.RoundGlyph(), radius = " + radius);
+    }
+    void draw() {
+        System.out.println("RoundGlyph.draw(), radius = " + radius);
+    }
+}
