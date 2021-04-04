68d4996b1797c3674bd5accfed32ab8c7a1c93d0
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 18:58:56 +0300

    Create Unicycle.java

---
+++ unit8/program17/Unicycle.java
@@ -0,0 +1,8 @@
+public class Unicycle extends Cycle{
+    public String toString() {
+        return "Unicycle";
+    }
+    public void balance() {
+        System.out.println("Balancing on " + this);
+    }
+}
