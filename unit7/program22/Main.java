ede15ffc6f99834870f73aaefeefbb24e2e90f18
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 14:33:50 +0300

    Create Main.java

---
+++ unit7/program22/Main.java
@@ -0,0 +1,9 @@
+//Create a final class and attempt to inherit from it
+
+public class Main {
+    public static void main(final String args[]) {
+        Test test = new Test();
+        test.d();
+        test.a = 10;
+    }
+}
