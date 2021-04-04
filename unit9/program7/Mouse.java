35c2e0d1d3a322d0885fc3367d646b5256bd6bbb
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 01:13:26 +0300

    Create Mouse.java

---
+++ unit9/program7/Mouse.java
@@ -0,0 +1,10 @@
+public class Mouse implements Rodent {
+
+    public void move() {
+        System.out.println("Mouse moves");
+    }
+
+    public void gnaw() {
+        System.out.println("Mouse gnaws");
+    }
+}
