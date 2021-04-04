0b52c6e4ddb301abb4d19e073dc203139684061a
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 14:16:25 +0300

    Create Test2.java

---
+++ unit7/program21/Test2.java
@@ -0,0 +1,8 @@
+public class Test2 extends Test{
+    public final void a() {
+        System.out.println("Overriding a");
+    }
+    public void b() {
+        System.out.println("Overriding b");
+    }
+}
