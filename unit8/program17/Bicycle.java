1966c11b1b22d7e9e590b36a468bc13b02500bc0
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 18:58:36 +0300

    Create Bicycle.java

---
+++ unit8/program17/Bicycle.java
@@ -0,0 +1,8 @@
+public class Bicycle extends Cycle{
+    public String toString() {
+        return "Bicycle";
+    }
+    public void balance() {
+        System.out.println("Balancing on " + this);
+    }
+}
