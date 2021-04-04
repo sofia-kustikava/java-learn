a023babb9ae1f78f01bdc5fc9cc9aa0cf07b867e
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-04 14:15:43 +0300

    Create Main.java

---
+++ unit7/program21/Main.java
@@ -0,0 +1,10 @@
+//Create a class with a final method. Inherit from that class and attempt
+//to overwrite that method.
+
+public class Main {
+    public static void main(final String args[]) {
+        Test2 test2 = new Test2();
+        test2.a();
+        test2.b();
+    }
+}
