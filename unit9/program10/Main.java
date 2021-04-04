eda0b929d4b3b2e65f9a5ba6450768265b3ed702
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 01:15:54 +0300

    Create Main.java

---
+++ unit9/program10/Main.java
@@ -0,0 +1,4 @@
+//Modify Musics.java by adding a Playable interface. Move the play( )
+//declaration from Instrument to Playable. Add Playable to the derived classes by
+//including it in the implement s list. Change tune( ) so that it takes a Playable instead of
+//an Instrument. 
