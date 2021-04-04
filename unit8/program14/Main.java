88c5f1dd58057b813a18fbe4466284aa3c7f7e9a
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 00:24:06 +0300

    Create Main.java

---
+++ unit8/program14/Main.java
@@ -0,0 +1,3 @@
+//Modify Exercise 9 so that it demonstrates the order of initialization of
+//the base classes and derived classes. Now add member objects to both the base and derived
+//classes and show the order in which their initialization occurs during construction. 
