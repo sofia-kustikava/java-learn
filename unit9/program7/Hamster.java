24116c62eca1c0bdaef50013d521df3dba66bac1
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 01:13:41 +0300

    Create Hamster.java

---
+++ unit9/program7/Hamster.java
@@ -0,0 +1,9 @@
+public class Hamster implements Rodent{
+    public void move() {
+        System.out.println("Hamster moves");
+    }
+
+    public void gnaw() {
+        System.out.println("Hamster gnaws");
+    }
+}
