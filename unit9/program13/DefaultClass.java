8d153a97f8b94fea296329715095fffcdbbb583c
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 15:48:16 +0300

    Create DefaultClass.java

---
+++ unit9/program13/DefaultClass.java
@@ -0,0 +1,11 @@
+public class DefaultClass implements Int4 {
+    public void int2() {
+        System.out.println("Interface 2");
+    }
+    public void int3() {
+        System.out.println("Interface 3");
+    }
+    public void int4() {
+        System.out.println("Interface 4");
+    }
+}
