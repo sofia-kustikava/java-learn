429437809429b082a22385c2acf09f1cb2887d75
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 15:47:53 +0300

    Create Main.java

---
+++ unit9/program13/Main.java
@@ -0,0 +1,12 @@
+//Create an interface, and inherit two new interfaces from that interface.
+//Multiply inherit a third interface from the second two.
+
+public class Main{
+    public static void main(String[] args) {
+        DefaultClass test = new DefaultClass();
+        test.int1();
+        test.int2();
+        test.int3();
+        test.int4();
+    }
+}
