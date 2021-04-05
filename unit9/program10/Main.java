a024eea59516f00df85c80a91b0ca2ff9472f8d1
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 15:17:55 +0300

    Update Main.java

--- unit9/program10/Main.java
+++ unit9/program10/Main.java
@@ -2,3 +2,25 @@
 //declaration from Instrument to Playable. Add Playable to the derived classes by
 //including it in the implement s list. Change tune( ) so that it takes a Playable instead of
 //an Instrument. 
+
+public class Main {
+    static void tune(Playable i) { 
+        i.play(Note.MIDDLE_C);
+    }
+
+    static void tuneAll(Playable[] e) {
+        for (Playable i : e)
+            tune(i);
+    }
+
+    public static void main(String[] args) {
+        Playable[] orchestra = {
+                new Windd(),
+                new Percussionn(),
+                new Stringedd(),
+                new Brasss(),
+                new Woodwind()
+        };
+        tuneAll(orchestra);
+    }
+}
