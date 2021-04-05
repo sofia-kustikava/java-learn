e67a24a09132c3dc0853ae47a490c4f83367353c
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-05 19:31:36 +0300

    Create DefaultClass.java

---
+++ unit9/program15/DefaultClass.java
@@ -0,0 +1,19 @@
+public class DefaultClass extends AnotherClass implements Int4 {
+
+    public void method3() {
+        System.out.println("Method 3");
+    }
+    public void method4() {
+        System.out.println("Method 4");
+    }
+    public void method5() {
+        System.out.println("Method 5");
+    }
+    public void method6() {
+        System.out.println("Method 6");
+    }
+    public void method7() {
+        System.out.println("Method 7");
+    }
+  
+}
