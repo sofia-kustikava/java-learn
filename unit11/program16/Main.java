12c32d32535a7b4a9b4e4351a62873e934a5de94
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-07 00:41:06 +0300

    Create Main.java

---
+++ unit11/program16/Main.java
@@ -0,0 +1,12 @@
+//Create a Set of the vowels. Working from UniqueWords.Java, count
+//and display the number of vowels in each input word, and also display the total number of
+//vowels in the input file.
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Vow vc = new Vow();
+        vc.vowelCounter();
+    }
+}
