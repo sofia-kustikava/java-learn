30fccee9415e0f09664ffa7e893cb73750ee289b
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-06 11:02:41 +0300

    Create Main.java

---
+++ unit10/program1/Main.java
@@ -0,0 +1,10 @@
+//Write a class named Outer that contains an inner class named Inner.
+//Add a method to Outer that returns an object of type Inner. In main( ), create and
+//initialize a reference to an Inner. 
+
+public class Main {
+    public static void main(String[] args) {
+        Outer mainTest = new Outer();
+        mainTest.test();
+    }
+}
