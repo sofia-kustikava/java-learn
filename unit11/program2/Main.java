915cf8406ecfe25657b327c92f47369cf8694fd2
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-07 20:22:36 +0300

    Delete unit11/program2 directory

--- unit11/program2/Main.java
+++
@@ -1,9 +0,0 @@
-//Define an object reference and initialize it to null. Try to call a method
-//through this reference. Now wrap the code in a try-catch clause to catch the exception. 
-
-public class Main {
-    public static void main(String[] args) {
-        ErrorEx errorEx = new ErrorEx();
-        errorEx.foo();
-    }
-}
