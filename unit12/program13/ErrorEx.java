1faa5fb7d67c1931dd2890c7e1f24308f35f1127
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-07 20:47:59 +0300

    Create ErrorEx.java

---
+++ unit12/program13/ErrorEx.java
@@ -0,0 +1,7 @@
+public class ErrorEx{
+    public void throwExc() throws Exception{
+        new Exception1().throwExc1();
+        new Exception2().throwExc2();
+        new Exception3().throwExc3();
+    }
+}
