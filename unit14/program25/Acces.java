09a9dcd7a30fd3223042b6edd11e8ad38289bddb
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-09 01:19:36 +0300

    Create Acces.java

---
+++ unit14/program25/Acces.java
@@ -0,0 +1,6 @@
+public class Acces {
+    public void publicMethod(){System.out.println("public");}
+    private void privateMethod(){System.out.println("private");}
+    void packageAccessMethod(){System.out.println("package access");}
+    protected void protectedMethod(){System.out.println("protected");}
+}
