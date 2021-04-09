947ad542ecf04a04f563f150ee1bb9817ab2237e
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-09 11:20:23 +0300

    Update RegisteredFactories.java

--- unit14/program24/RegisteredFactories.java
+++ unit14/program24/RegisteredFactories.java
@@ -2,7 +2,7 @@
 
 public class RegisteredFactories {
     public static void main(String[] args) {
-        public static final Part NullPart =new Part();
+        public static final Part NULL =new Part();
         for(int i = 0; i < 10; i++)
             System.out.println(NullPart.createRandom());
     }
