0549163ef3ea7b4f124ac0b9e7683127e743403c
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-11 22:52:09 +0300

    Update Division.java

--- unit20/program2/Division.java
+++ unit20/program2/Division.java
@@ -2,15 +2,30 @@
 
 @ExtractInterface("IDivision")
 public class Division { 
-    public int divide(int x, int y) {
-        int total = 0;
-        for(int i = 0; i < x; i++)
-            total = add(total, y);
-        return total;
-    }
-    private int add(int x, int y) { return x / y; }
-    public static void main(String[] args) {
-        Division m = new Division();
-        System.out.println("15/5 = " + m.divide(15, 5));
-    }
+    public int divide(int x, int y) throws ArithmeticException {
+		if(y == 0) throw new ArithmeticException("Divide by zero");
+		int diff = subtract(x, y);
+		if(diff < 0) return 0;
+		if(diff == 0) return 1;
+		int result = 0;
+		while(diff >= 0) {
+			result++;
+			diff = subtract(diff, y);				
+		}
+		return result;
+	}
+	private int subtract(int x, int y) { return x - y; }
+	public static void main(String[] args) {
+		try {
+		Divider d = new Divider();
+		print("11/16 = " + d.divide(11, 16));		
+		print("10/10 = " + d.divide(10,10));
+		print("10/1 = " + d.divide(10,1));
+		print("10/2 = " + d.divide(10,2));
+		print("10/3 = " + d.divide(10,3));
+		print("10/0 = " + d.divide(10,0));		
+		} catch(Exception e)  {
+			print(e);
+		}
+	}
 }
