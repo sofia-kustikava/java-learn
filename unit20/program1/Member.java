9eede6723aa810f027e300ba5f550ca4b4e319cb
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-11 21:50:54 +0300

    Create Member.java

---
+++ unit20/program1/Member.java
@@ -0,0 +1,15 @@
+@DBTable(name = "MEMBER")
+public class Member {
+    @SQLString(30) String firstName;
+    @SQLString(50) String lastName;
+    @SQLInteger Integer age;
+    @SQLDecimal(10) Integer height;
+    @SQLDate(20) Long birthday;
+    @SQLString(value = 30, constraints = @Constraints(primaryKey = true)) String handle;
+    static int memberCount;
+    public String getHandle() { return handle; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String toString() { return handle; }
+    public Integer getAge() { return age; }
+}
