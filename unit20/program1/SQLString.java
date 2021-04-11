6533c1e871a4ab88095014e60bc802894786ee24
Sofia Kustikova <81553793+sofia-kustikava@users.noreply.github.com>
2021-04-11 21:53:25 +0300

    Create SQLString.java

---
+++ unit20/program1/SQLString.java
@@ -0,0 +1,9 @@
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SQLString {
+    int value() default 0;
+    String name() default "";
+    Constraints constraints() default @Constraints;
+}
